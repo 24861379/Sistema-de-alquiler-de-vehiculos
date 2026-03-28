9c7708fd490ab2c7251f7d21e2c0bbd06731ee93
Nicole <0nicolemoreno1@gmail.com>
2026-03-27 20:55:05 -0500

    actualización

--- RentCar-API/src/main/java/com/RentCar/RentCar/services/ClienteService.java
+++ RentCar-API/src/main/java/com/RentCar/RentCar/services/ClienteService.java
@@ -1,12 +1,17 @@
 package com.RentCar.RentCar.services;
 
+import com.RentCar.RentCar.dto.dtoRequest.ActualizarClienteDtoRequest;
 import com.RentCar.RentCar.dto.dtoRequest.CrearClienteDtoRequest;
+import com.RentCar.RentCar.entity.ClienteEntity;
 import com.RentCar.RentCar.repository.ClienteRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClienteService {
+
     private ClienteRepository clienteRepository;
 
     public ClienteService(ClienteRepository clienteRepository) {
@@ -20,6 +25,32 @@ public class ClienteService {
                 request.getApellidoCliente(),
                 request.getEmail(),
                 request.getTelefono(),
-                request.getDireccion());
+                request.getDireccion()
+        );
+    }
+
+    // 🔹 LISTAR
+    public List<ClienteEntity> listarClientes() {
+        return clienteRepository.findAll();
+    }
+
+    // 🔹 ACTUALIZAR
+    public void actualizarCliente(ActualizarClienteDtoRequest dto){
+        ClienteEntity cliente = clienteRepository.findById(dto.getId())
+                .orElseThrow(() -> new RuntimeException("Cliente no encontrado"));
+
+        cliente.setNombreCliente(dto.getNombreCliente());
+        cliente.setApellidoCliente(dto.getApellidoCliente());
+        cliente.setDocumento(dto.getDocumento());
+        cliente.setEmail(dto.getEmail());
+        cliente.setTelefono(dto.getTelefono());
+        cliente.setDireccion(dto.getDireccion());
+
+        clienteRepository.save(cliente);
+    }
+
+    // 🔹 ELIMINAR
+    public void eliminarCliente(Long id){
+        clienteRepository.deleteById(id);
     }
 }
